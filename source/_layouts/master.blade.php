4b998603e9679b010ad80e1f86850da5e7700a0a
Kevin Chandra <kacol.bot@gmail.com>
2025-03-14 22:59:50 +0700

    WIP mobile layout

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -12,7 +12,48 @@
     @stack('after_styles')
 </head>
 <body class="antialiased font-gotham-rounded text-body">
+<header class="rounded-b-3xl bg-primary fixed top-0 z-20 w-full">
+    <div class="container mx-auto px-4">
+        <div class="flex py-4 items-center gap-x-4">
+            <img src="/assets/images/logo@2x.png" alt="" class="h-8">
+            <div class="font-bold text-base">
+                • For sensitive, dry, atopic skin
+            </div>
+        </div>
+    </div>
+</header>
 @yield('content')
+<footer class="bg-[#9ea4ac] py-8 text-white">
+    <div class="container mx-auto px-4">
+        <div class="grid grid-cols-2 gap-x-4">
+            <div class="col-span-1 text-center">
+                FOLLOW US
+                <div class="mt-2 flex flex-row justify-center gap-x-4">
+                    <a href="" class="w-1/4">
+                        <img src="/assets/images/icons/icon-instagram.png" alt="" class="w-6 mx-auto">
+                    </a>
+                    <a href="" class="w-1/4">
+                        <img src="/assets/images/icons/icon-instagram.png" alt="" class="w-6 mx-auto">
+                    </a>
+                    <a href="" class="w-1/4">
+                        <img src="/assets/images/icons/icon-instagram.png" alt="" class="w-6 mx-auto">
+                    </a>
+                </div>
+            </div>
+            <div class="col-span-1 text-center">
+                FIND US
+                <div class="mt-2 flex flex-row justify-center gap-x-4">
+                    <a href="" class="w-1/4">
+                        <img src="/assets/images/icons/icon-instagram.png" alt="" class="w-6 mx-auto">
+                    </a>
+                    <a href="" class="w-1/4">
+                        <img src="/assets/images/icons/icon-instagram.png" alt="" class="w-6 mx-auto">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/swiper@11.2.4/swiper-bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.min.js"></script>
 <script>
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,24 +1,14 @@
 @extends('_layouts.master')
 
 @section('content')
-    <header class="rounded-b-3xl bg-primary fixed top-0 z-20 w-full">
-        <div class="container mx-auto px-4">
-            <div class="flex flex-row py-10 items-center gap-x-4">
-                <img src="/assets/images/logo@2x.png" alt="" class="h-20">
-                <div class="font-bold text-4xl">
-                    • For sensitive, dry, atopic skin
-                </div>
-            </div>
-        </div>
-    </header>
-    <section class="mt-20">
+    <section class="mt-10">
         <div class="relative">
             <img src="/assets/images/hero-banner.jpeg" alt="" class="w-full">
             <div class="absolute top-0 left-0 w-full h-full flex items-center">
                 <div class="container mx-auto px-4">
-                    <h1 class="text-5xl text-white">
+                    <h1 class="text-2xl text-white">
                         3 Langkah Perawatan untuk
-                        <span class="font-bold text-6xl block" style="text-shadow: 1px 1px rgba(0, 0, 0, .7);">
+                        <span class="font-bold text-2xl block" style="text-shadow: 1px 1px rgba(0, 0, 0, .7);">
                             Kulit Sensitif, Kering & Rentan Eksim
                         </span>
                     </h1>
@@ -26,42 +16,92 @@
             </div>
         </div>
     </section>
-    <div style="background: url(/assets/images/bg-ripple.png) no-repeat center bottom;background-size: cover"
-         class="py-20">
-        <section class="py-8">
+    <div style="background: url(/assets/images/bg-ripple.png) no-repeat center bottom;background-size: cover" class="pt-10">
+        <section>
             <div class="container mx-auto px-4 text-center">
-                <h1 class="text-5xl font-bold mb-8">
+                <h1 class="text-xl font-bold mb-8">
                     Nuudo percaya bahwa tubuh yang sehat dimulai dengan <span
                             class="text-primary">skin barrier yang sehat.</span>
                 </h1>
-                <p class="text-3xl mb-4">
+                <p class="text-lg mb-4">
                     Skin barrier adalah lapisan terluar kulit manusia,
                     yang memiliki fungsi utama untuk menjaga kelembapan kulit dan melindungi tubuh dari potensi iritasi.
                 </p>
                 <img src="/assets/images/skin-barrier.png" alt="" class="mx-auto w-40 mb-4">
-                <p class="text-3xl mb-4 font-bold">
+                <p class="text-lg mb-4 font-bold">
                     Pada kondisi kulit normal, skin barrier umumnya tidak mudah rusak. Kondisi skin barrier yang mudah
                     rusak
                     sering ditemui pada kulit:
                 </p>
                 <div>
-                    carousel
+                    skin carousel
                 </div>
             </div>
         </section>
-        <section>
+        <section class="mt-12">
             <div class="container mx-auto px-4">
                 <div class="mx-auto max-w-[800px]">
-                    <h2 class="text-3xl text-center">
+                    <h2 class="text-lg text-center">
                         <strong class="text-primary">Nuudo Dermaprotect</strong> diformulasikan khusus
                         untuk menjaga skin barrier kulit sensitif, kering, dan rentan eksim, menggunakan campuran
-                        <strong>plant
-                            butter
-                            dan oil.</strong>
+                        <strong>plant butter dan oil.</strong>
                     </h2>
                 </div>
-                <img src="/assets/images/natural-ingredients.png" alt="" class="mx-auto h-40 mt-8">
+                <img src="/assets/images/natural-ingredients.png" alt="" class="mx-auto mt-8">
+            </div>
+            <img src="/assets/images/product-showcase.png" alt="" class="w-full">
+        </section>
+    </div>
+    <div style="background: url(/assets/images/bg-ripple.png) no-repeat center bottom;background-size: cover" class="pt-10">
+        <section>
+            <div>
+                showcase carousel
+            </div>
+            <div class="mt-8">
+                <div class="container mx-auto px-4">
+                    <div class="rounded-full border-primary border-4 bg-white flex items-center p-2 gap-x-2">
+                        <img src="/assets/images/icons/bulb-icon.png" alt="" class="w-20">
+                        <div class="text-center flex-grow text-xs">
+                            <strong class="block text-base">
+                                TAHUKAH ANDA?
+                            </strong>
+                            Kandungan vegetable glycerin di Nuudo Dermaprotect Gentle Cleanser lebih tinggi dari
+                            kandungan
+                            air
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="mt-12 pb-8">
+            <h3 class="font-bold text-2xl text-center">
+                Rasakan Kebaikan Nuudo
+            </h3>
+            <div>
+                testimonial carousel
+            </div>
+            <div class="container mx-auto px-4">
+                <div class="text-center">
+                    Nuudo accommodates the needs of all types of skin rom normal, sensitive, dry and atopic skin
+                </div>
+            </div>
+            <h3 class="font-bold text-2xl text-center">
+                Other Products:
+            </h3>
+            <div>
+                other products carousel
             </div>
         </section>
     </div>
+    <section title="outro">
+        <div class="relative">
+            <img src="/assets/images/all-products.jpeg" alt="" class="w-full">
+            <div class="absolute bottom-0 left-0 w-full text-center py-4">
+                <img src="/assets/images/logo-nuudo@2x.png" alt="" class="w-40 mx-auto">
+                <h4 class="text-xl mt-2">
+                    Where Natural Goodness Meets Everyday Care
+                </h4>
+            </div>
+        </div>
+    </section>
 @endsection
